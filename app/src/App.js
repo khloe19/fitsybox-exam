2ada97dc2a28f143abfb3288c050d87531e86f0b
khloe19 <67881077+khloe19@users.noreply.github.com>
2020-11-05 20:05:59 +1300

    tidy up bits and pieces

--- app/src/App.js
+++ app/src/App.js
@@ -8,13 +8,18 @@ class App extends React.Component {
       super(props)
       this.state = { products: [],
                      id: '',
-                     quantity: 0}  
+                     price: 0,
+                     totalAmout: 0,
+                     quantity: 0,
+                     orderAdded: false}  
+
       this.handleProductChange = this.handleProductChange.bind(this);
       this.handleSubmit = this.handleSubmit.bind(this);
       this.handleInputChange = this.handleInputChange.bind(this);
     };
 
     componentDidMount(){
+      // Retrieve products
       this.fetchOptions()
     }
 
@@ -23,26 +28,36 @@ class App extends React.Component {
       fetch(apiUrl, {
           method: "GET",
           headers: { 'Content-Type': ' application/json' }
-         }).then((response) => { return response.json() })
-           .then((data) => {values = data;
+         })
+         .then((response) => { return response.json() })
+         .then((data) => {values = data;
                          this.setState({ products: values });
-                      })
+         })
     }
 
+    // Product selection
     handleProductChange(event) {
-      this.setState({
-        id: event.target.value
+      const selectedProduct = JSON.parse(event.target.value)
+      this.setState({          
+        id: selectedProduct.id,
+        price: selectedProduct.price,
+        totalAmout: this.state.quantity * selectedProduct.price 
       })
     }
 
+    // Quantity input
     handleInputChange(event) {
+      let quantity = event.target.value 
       this.setState({
-        quantity: event.target.value
+        quantity: quantity,
+        totalAmout: quantity * this.state.price
       })
     }
 
+    // Create order
     handleSubmit() {
-      const data = [{id:this.state.id, quantity:this.state.quantity}];
+      const data = [{product_id: this.state.id, 
+                    quantity: this.state.quantity}];
 
       fetch('http://localhost:3001/api/orders', {
         method: 'POST',
@@ -52,8 +67,8 @@ class App extends React.Component {
         }
       }).then(response => {
         if (response.status === 200) {
-          console.log(response);
-          return response;
+            this.setState({ orderAdded: true })
+            return response;
         } else {
          console.log('Something went wrong');
         }
@@ -65,21 +80,28 @@ class App extends React.Component {
 
       return (
         <div>
-          <h1>Mels Store</h1>
-          <div className="dropdown">
-              <span>Choose a Product</span>
-              <select
-                onChange={this.handleProductChange}>
-                {products.map(product => <option key={product.id}>{product.name}</option>)}
-              </select>              
+          <h1>FitsyBox Store</h1>
+          <div className="container">
+            <div className="dropdown">
+                <span>Choose a product: </span>
+                <select
+                    onChange={this.handleProductChange}>
+                    {products.map(product => <option key={product.id} value={JSON.stringify(product)}>{product.id}-{product.name}</option>)}
+                </select>              
+            </div>
+            <div className="quantity-text">
+                <span>Enter quantity: </span>
+                <input type="number" style={{paddingLeft: "5px"}} onChange={this.handleInputChange}/>
+            </div>
+            <div className="amountContainer">
+                <span>Total amount: </span>
+                <span className="amount">{this.state.totalAmout}</span>
+            </div>          
+            <button className="createOrder-btn" type="button" disabled={this.state.totalAmout === 0} onClick={this.handleSubmit}>
+                Create order
+            </button>
+            {this.state.orderAdded && <p>Your order make my day! Thank you. :)</p>}
           </div>
-          <div className="dropdown">
-              <span>Enter quantity</span>
-              <input type="number" onChange={this.handleInputChange}/>
-          </div>
-          <button type="button" onClick={this.handleSubmit}>
-            Create Order
-          </button>
         </div>
       )}
 };
